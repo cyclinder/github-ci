3860b5030e5174025de1271280420b1e66d5ea0e
cyclinder <qifeng.guo@daocloud.io>
2022-03-30 16:24:08 +0800

    add sync helm chart and push it to new branch -1

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-workdir="home/runner/work/github-ci/github-ci"
-time_now=$(date +"%Y-%m-%d")
-branch_name="sync_chart"_$(time_now)
+set -e
+
+time_now=`date +"%Y-%m-%d"`
+branch_name="sync_chart_"${time_now}
+
 
 function get_chart {
     echo "git clone and copy chart file"
     git clone https://github.com/k8snetworkplumbingwg/helm-charts.git
-    cp -rf helm-charts/multus/* $(workdir)/charts/multus-origin
+    cp -rf helm-charts/multus/* ${GITHUB_WORKSPACE}/charts/multus-origin
+    ls ${GITHUB_WORKSPACE}/charts/multus-origin
 }
 
 function update_chart() {
-    echo "update chart and push to a new branch: $(branch_name)"
-    cd $workdir
-    git checkout -b $branch_name
+    echo "ready update chart and push to a new branch: ${branch_name}"
+    cd ${GITHUB_WORKSPACE}
+    git checkout -b ${branch_name}
     git config user.name github-actions
     git config user.email github-actions@github.com
+    echo `pwd`
     git add .
-    git commit -m "Automatic: update multus helm chart($(date)"
-    git push
+    git commit -m "Automatic: update multus helm chart(${date}"
+    git push --set-upstream origin ${branch_name}
 }
 
 get_chart
